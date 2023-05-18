5ad72311908c1814fd6be08173c12f1191286dd5
Eng-Hady <hadyronaldo2030@gmail.com>
2023-05-18 04:14:27 +0300

    add Home v11

--- asstes/js/index-login.js
+++ asstes/js/index-login.js
@@ -1,11 +1,5 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
-
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
-});
-
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
+$(document).ready(function() {
+  $("#sign-up-btn,#sign-in-btn").click(function() {
+    $(".container").toggleClass("sign-up-mode");
+  });
 });
