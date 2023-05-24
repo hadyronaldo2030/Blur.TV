46a12ac7b244cd2c35219364be2fbbfc8abae112
Eng-Hady <hadyronaldo2030@gmail.com>
2023-05-24 22:47:27 +0300

    template movies blur.tv last-v

--- asstes/js/shared.js
+++ asstes/js/shared.js
@@ -95,26 +95,24 @@ $(document).ready(function() {
 });
 // =========================== Icon Top  ===========================
 
-// Get the button
-let mybutton = document.getElementById("myBtn");
-    
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-	mybutton.style.display = "block";
-  } else {
-	mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
+$(document).ready(function() {
+    // Get the button
+    let mybutton = $("#myBtn");
+  
+    // When the user scrolls down 20px from the top of the document, show the button
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 20) {
+        mybutton.css("display", "block");
+      } else {
+        mybutton.css("display", "none");
+      }
+    });
+  
+    // When the user clicks on the button, scroll to the top of the document
+    $("#myBtn").click(function() {
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+  });
 
 //============================= Bubble Trailer =============================
 	// show bop img 
