69b6f51de6534b11ef7af4b0830e33151a6938a0
Eng-Mohamed Mahmoud Salah <64635889+Eng-Mohamed-Mado@users.noreply.github.com>
2023-05-28 06:11:43 +0300

    Update index-home.js

--- asstes/js/index-home.js
+++ asstes/js/index-home.js
@@ -7,7 +7,6 @@ var imgWidth = 200;
 var imgHeight = 400;
 
 
-// var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
 var bgMusicControls = true;
 
 
